f37271a4f7f799baa6975eda3b6c4cc99d6c7d8f
yashvarmora2306 <yashpatel23062002@gmail.com>
2024-01-16 12:32:23 +0530

    -

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import Services from './Services';
+import SpacialOffer from './SpacialOffer';
+import CustomerReviews from './CustomerReviews';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+
+export {
+    Hero,
+    PopularProducts,
+    Services,
+    SpacialOffer,
+    Subscribe,
+    SuperQuality,
+    CustomerReviews,
+    Footer
+}
